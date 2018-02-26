b088ff8fd885b7ba0cfa620c30c9df281cebfeec
Igor_Sysoev <Igor_Sysoev@epam.com>
2018-02-26 18:59:40 +0300

    added dnd

--- src/App.js
+++ src/App.js
@@ -7,13 +7,17 @@ import {ConnectedRouter} from 'react-router-redux'
 import history from './history'
 import './config'
 import './mocks/index'
+import {DragDropContextProvider} from 'react-dnd'
+import HTML5Backend from 'react-dnd-html5-backend'
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
         <ConnectedRouter history={history}>
-          <Root/>
+          <DragDropContextProvider backend={HTML5Backend}>
+            <Root/>
+          </DragDropContextProvider>
         </ConnectedRouter>
       </Provider>
     )
